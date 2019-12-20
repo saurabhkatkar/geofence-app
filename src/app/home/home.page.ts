ce6b8d6dcb62bf45e68d8df6911946484e410ecd
Saurabh Katkar <saurabh.katkar103@gmail.com>
2019-12-20 16:29:22 +0530

    Adding some changes to tsconfig.json for ES5 error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { Geofence } from '@ionic-native/geofence/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public platform: Platform,private geofence: Geofence) {
+    // initialize the plugin
+    geofence.initialize().then(
+      // resolved promise does not return a value
+      () => console.log('Geofence Plugin Ready'),
+      (err) => console.log(err)
+    )
+    this.addFences();
+  }
+  addFences(){
+    console.log("this is working");
+    this.addGeofence();
+  }
+
+  private addGeofence() {
+    //options describing geofence
+    let fence = {
+      id: '69ca1b88-6fbe-4e80-a4d4-ff4d3748acdb', //any unique ID
+      latitude:       37.285951, //center of geofence radius
+      longitude:      -121.936650,
+      radius:         100, //radius to edge of geofence in meters
+      transitionType: 3, //see 'Transition Types' below
+      notification: { //notification settings
+          id:             1, //any unique ID
+          title:          'You crossed a fence', //notification title
+          text:           'You just arrived to Gliwice city center.', //notification body
+          openAppOnClick: true //open app when notification is tapped
+      }
+      
+    }
+  
+    this.geofence.addOrUpdate(fence).then(
+       () => console.log('Geofence added'),
+       (err) => console.log('Geofence failed to add')
+     );
+  }
 
 }
